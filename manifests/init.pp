88e699028c0b5148f990715d1352eb3d0df9b6ba
mergwyn <gary@theclarkhome.com>
2018-10-20 15:16:19 +0100

    Add worker classes

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,21 +8,11 @@
 #
 # Document parameters here.
 #
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
+# * package_manage
+# * service_manage
+# * service_enable
+# * user
+# * group
 #
 # Examples
 # --------
@@ -42,8 +32,21 @@
 #
 # Copyright 2018 Your name here, unless otherwise noted.
 #
-class jackett {
+class jackett (
+  Boolean $package_manage = true,
+  Boolean $service_manage = true,
+  Boolean $service_enable = true,
+  String  $user = 'jackett',
+  String  $group = 'jackett',
+  ) {
 
+  contain jackett::config
+  contain jackett::install
+  contain jackett::service
+
+  Class['::jackett::install']
+  -> Class['::jackett::config']
+  ~> Class['::jackett::service']
 
 }
-# vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
+# vim: nu tabstop=8 expandtab shiftwidth=2 softtabstop=2
