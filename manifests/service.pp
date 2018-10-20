aa179b0462ef38abc01eb283707834b36b71f1b8
mergwyn <gary@theclarkhome.com>
2018-10-20 15:16:55 +0100

    convert to template for systemd

--- manifests/service.pp
+++ manifests/service.pp
@@ -13,30 +13,7 @@ class jackett::service {
     systemd::unit_file { 'jackett.service':
       enable  => $jackett::service_enable,
       active  => true,
-      content => '[Unit]
-  Description=Jackett Daemon
-  After=network.target
-
-  [Service]
-  User=media
-  Restart=always
-  RestartSec=5
-  Type=simple
-  ExecStart=/usr/bin/mono --debug /opt/Jackett/JackettConsole.exe --NoRestart
-  TimeoutStopSec=20
-
-  [Install]
-  WantedBy=multi-user.target
-  '
-      }
-
-    service { 'jackett':
-      ensure     => $jackett::service_ensure,
-      name       => $jackett::service_name,
-      provider   => $jackett::service_provider,
-      hasstatus  => true,
-      hasrestart => true,
-    }
+      content => content => template('jackett/systemd.erb'),
   }
 }
 # vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
