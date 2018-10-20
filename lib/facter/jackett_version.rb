ea322ed81cabd688ba22c5b6c84138a0821b5c2f
mergwyn <gary@theclarkhome.com>
2018-10-20 11:45:18 +0100

    Initial version

---
+++ lib/facter/jackett_version.rb
@@ -0,0 +1,19 @@
+require 'facter'
+
+# Default for non-Linux nodes
+#
+Facter.add(:jackett_version) do
+    setcode do
+        nil
+    end
+end
+
+# Linux
+#
+Facter.add(:jackett_version) do
+    confine :kernel  => :linux
+    setcode do
+        Facter::Util::Resolution.exec("wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep -E \/tag\/ | awk -F '[><]' '{print $3}'")
+    end
+end
+
