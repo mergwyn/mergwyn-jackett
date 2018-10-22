08af894150ae49fa75c39a785cc9459ff2312037
mergwyn <gary@theclarkhome.com>
2018-10-22 16:51:31 +0100

    Fix refer to user

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,28 +1,26 @@
 #
 class jackett::install {
 
-  #TODO get /opt from variable
   $install_path   = $::jackett::install_path
   $package_name   = 'Jackett.Binaries.Mono'
   $package_version = $::jackett_version
   $repository_url = 'https://github.com/Jackett/Jackett/releases/download/'
   $package_source = "${repository_url}/${package_version}/${package_name}.tar.gz"
-  $archive_name   = "${package_name}-${package_version}.tgz"
+  $archive_name   = "${install_path}/${package_name}-${package_version}.tar.gz"
 
   if $jackett::package_manage {
-    #TODO set user from variable
-    archive { 'jackett_package_zip':
-      path         => "${install_path}/${package_name}_${package_version}.tar.gz",
+    archive { 'jackett_package_tar':
+      path         => $archive_name,
       source       => $package_source,
-      user         => $::jacket::user,
-      group        => $::jacket::group,
+      user         => $::jackett::user,
+      group        => $::jackett::group,
       extract      => true,
-      extract_path => $install_path,
-      creates      => "${install_path}/Jackett",
+      extract_path => $archive_name,
+      creates      => $archive_name,
       cleanup      => false,
       #TODO reference to mono
       #require      => Class['mono'],
     }
   }
 }
-# vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
+# vim: number tabstop=8 expandtab shiftwidth=2 softtabstop=2
