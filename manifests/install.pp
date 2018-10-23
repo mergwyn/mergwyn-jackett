5d9470fb20c9c1f6dafb7eebd0be92ddc41b137b
mergwyn <gary@theclarkhome.com>
2018-10-23 12:07:09 +0100

    remove creates

--- manifests/install.pp
+++ manifests/install.pp
@@ -16,7 +16,7 @@ class jackett::install {
       group        => $::jackett::group,
       extract      => true,
       extract_path => $install_path,
-      creates      => $archive_name,
+      #creates      => $archive_name,
       cleanup      => false,
       #TODO reference to mono
       #require      => Class['mono'],
