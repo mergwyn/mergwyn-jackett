cd74c766af4df6997dfbf109f331d39f2c82312e
mergwyn <gary@theclarkhome.com>
2018-10-23 15:19:50 +0100

    Create extract dir

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,17 +3,19 @@ class jackett::install {
 
   #TODO tidy up old versions
 
-  $package_name   = 'Jackett.Binaries.Mono'
+  $package_name    = 'Jackett.Binaries.Mono'
   $package_version = $::jackett_version
-  $install_path   = $::jackett::install_path
-  $extract_dir    = "${install_path}/Jackett-${package_version}"
-  $creates        = "${extract_dir}/Jackett"
-  $repository_url = 'https://github.com/Jackett/Jackett/releases/download/'
-  $package_source = "${repository_url}/${package_version}/${package_name}.tar.gz"
-  $archive_name   = "${package_name}-${package_version}.tar.gz"
-  $archive_path   = "${install_path}/${archive_name}"
+  $install_path    = $::jackett::install_path
+  $extract_dir     = "${install_path}/Jackett-${package_version}"
+  $creates         = "${extract_dir}/Jackett"
+  $repository_url  = 'https://github.com/Jackett/Jackett/releases/download/'
+  $package_source  = "${repository_url}/${package_version}/${package_name}.tar.gz"
+  $archive_name    = "${package_name}-${package_version}.tar.gz"
+  $archive_path    = "${install_path}/${archive_name}"
 
   if $jackett::package_manage {
+    file { $extract_dir: ensure => dir, }
+
     archive { $archive_name:
       path         => $archive_path,
       source       => $package_source,
@@ -24,11 +26,11 @@ class jackett::install {
       #TODO reference to mono
       #require      => Class['mono'],
     }
-  }
-  file { '/opt/Jackett':
-    ensure => 'link',
-    target => $creates,
-    subscribe => Archive[$archive_path],
+    file { '/opt/Jackett':
+      ensure    => 'link',
+      target    => $creates,
+      subscribe => Archive[$archive_path],
+    }
   }
 }
 # vim: number tabstop=8 expandtab shiftwidth=2 softtabstop=2
