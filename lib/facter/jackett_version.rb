36cb243b8babf0ab949f13992ec92ae5b2a9bd0e
mergwyn <gary@theclarkhome.com>
2018-12-15 18:44:47 +0000

    convert to pdk module

--- lib/facter/jackett_version.rb
+++ lib/facter/jackett_version.rb
@@ -3,17 +3,16 @@ require 'facter'
 # Default for non-Linux nodes
 #
 Facter.add(:jackett_version) do
-    setcode do
-        nil
-    end
+  setcode do
+    nil
+  end
 end
 
 # Linux
 #
 Facter.add(:jackett_version) do
-    confine :kernel  => :linux
-    setcode do
-        Facter::Util::Resolution.exec("wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep -E \/tag\/ | awk -F '[><]' '{print $3}'")
-    end
+  confine kernel: :linux
+  setcode do
+    Facter::Util::Resolution.exec("wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep -E \/tag\/ | awk -F '[><]' '{print $3}'")
+  end
 end
-
