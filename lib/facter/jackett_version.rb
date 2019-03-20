47d87b9debe2c98ed8b2bb34cdf1604f96d780d5
mergwyn <gary@theclarkhome.com>
2019-03-20 19:44:39 +0000

    fix escaping backslash

--- lib/facter/jackett_version.rb
+++ lib/facter/jackett_version.rb
@@ -13,6 +13,6 @@ end
 Facter.add(:jackett_version) do
   confine kernel: :linux
   setcode do
-    Facter::Util::Resolution.exec("curl -sI https://github.com/Jackett/Jackett/releases/latest | grep -Po 'tag\\\\/\\\\K(v\\\\S+)'")
+    Facter::Util::Resolution.exec("curl -sI https://github.com/Jackett/Jackett/releases/latest | grep -Po 'tag\\/\\K(v\\S+)'")
   end
 end
