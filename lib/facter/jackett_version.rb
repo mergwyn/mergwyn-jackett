5c952de10d99db2e26d3866e376e0bb5bbd20fa9
mergwyn <gary@theclarkhome.com>
2020-03-30 10:58:44 +0100

    Allow for location and Location

--- lib/facter/jackett_version.rb
+++ lib/facter/jackett_version.rb
@@ -14,6 +14,6 @@ Facter.add(:jackett_version) do
   confine kernel: :linux
   setcode do
     # Facter::Util::Resolution.exec("curl -sI https://github.com/Jackett/Jackett/releases/latest | grep -Po 'tag\\/\\K(v\\S+)'")
-    Facter::Util::Resolution.exec("curl -sI https://github.com/Jackett/Jackett/releases/latest | sed -n '/^Location:/s;^.*tag/v\\(.*\\)$;\\1;p'")
+    Facter::Util::Resolution.exec("curl -sI https://github.com/Jackett/Jackett/releases/latest | sed -n '/^[lL]ocation:/s;^.*tag/v\\(.*\\)$;\\1;p'")
   end
 end
